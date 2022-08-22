c09a114846718c2830531ea0a2d3ff1b94636673
AngelaCuba <cpf.cuba13394732@gmail.com>
2022-08-22 00:11:37 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FComponent } from '../fcomponent/fcomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    AppComponent,
+    FComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
